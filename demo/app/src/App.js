aa74748b6a82ea7f2fc296e5e8af81e6f41e7aaa
Simon Li <lsimon@ca.ibm.com>
2018-01-22 15:30:31 -0500

    init code for the demo app

---
+++ demo/app/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import BuForm from './lib/buForm.js';
+import 'bulma/css/bulma.css';
+
+import fields from './fields.js';
+
+import logo from './logo.svg';
+import './App.css';
+import './lib/bulma-calendar.css';
+
+const App = observer(class _App extends Component {
+
+	// form elements data (supposedly from server)
+	constructor() {
+		super();
+
+		this.data = observable({
+			input1: 'input1 text',
+			input2: 'input2 text',
+			checkbox1: true,
+			checkbox2: true,
+			select1: 'sel2',
+			datepicker1: '',
+			textarea1: 'textarea1 text',
+			radio1: 'radio11',
+			radio2: 'radio21'
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+
+				<div className="container">
+					<section className="section">
+						<BuForm name={'testForm'} fields={fields} data={this.data} alignment="is-horizontal" debug />
+					</section>
+				</div>
+			</div>
+		);
+	}
+});
+
+export default App;
---
+++ demo/app/src/lib/buForm.js
@@ -0,0 +1,234 @@
+import React from 'react';
+import { action, autorun, toJS } from 'mobx';
+import { observer } from 'mobx-react';
+import DatePicker from './datepicker.js';
+
+/**
+ * label component wrapper
+ * @param {string} label Label
+ * @param {string} size Size
+ * @returns {object} label node
+ */
+function Label({ label, size = 'is-normal', alignment }) {
+    if (alignment === 'is-horizontal') {
+        return <div>
+            <label className={`field-label label ${size}`}>{label}</label>
+        </div>;
+    }
+
+    return <label className={`label ${size}`}>{label}</label>;
+}
+
+const BuForm = observer(class _BuForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.data = this.props.data;
+        this.setPropValue = action((prop, value) => {
+            this.data[prop] = value;
+        });
+    }
+
+    componentDidMount() {
+        this.dpInstances = [];
+
+        const flattened = this.props.fields.reduce((prev, curr) => prev.concat(curr), []);
+        const datepickers = flattened.filter(elem => elem.type === 'datepicker');
+
+        datepickers.forEach((elem) => {
+            const domNode = document.getElementsByName(elem.name)[0];
+            const dp = new DatePicker(domNode, {
+                onSelect: val => this.setPropValue(elem.name, val),
+                dataFormat: 'yyyy/mm/dd',
+            });
+            this.dpInstances.push(dp);
+        });
+
+        if (this.props.debug) {
+            autorun(() => {
+                console.log('[Mobx] data store', toJS(this.data)); // eslint-disable-line no-console
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.dpInstances.length > 0) {
+            this.dpInstances.forEach(dp => dp && dp.destroy());
+        }
+    }
+
+    BmText(element) {
+        const { name, label, css, placeholder } = element;
+        return [
+            <Label key="1" label={label} alignment={this.props.alignment} />,
+            <div key="2" className="field-body">
+                <div className="field">
+                    <div className="control">
+                        <input name={name} className={`input ${css}`} type="text" placeholder={placeholder}
+                            value={this.data[name]} onChange={ev => this.setPropValue(name, ev.target.value)} />
+                    </div>
+                </div>
+            </div>
+        ];
+    }
+
+    BmCheck(element) {
+        const { name, label, css, options } = element;
+        return [
+            <Label key="1" label={label} alignment={this.props.alignment} />,
+            <div key="2" className="field-body">
+                <div className="field">
+                    <div className="control">
+                        {
+                            options.map((option, key) => (
+                                <label key={key} className="checkbox">
+                                    <input name={name} className={css} type="checkbox"
+                                        value={this.data[option.name]}
+                                        checked={this.data[option.name]}
+                                        onChange={ev => this.setPropValue(option.name, ev.target.checked)} />
+                                    {option.label}
+                                </label>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        ];
+    }
+
+    BmSelect(element) {
+        const { name, label, css, options } = element;
+
+        return [
+            <Label key="1" label={label} alignment={this.props.alignment} />,
+            <div key="2" className="field-body">
+                <div className="field">
+                    <div className="control">
+                        <div className="select is-fullwidth">
+                            <select name={name} className={css} value={this.data[name]} onChange={ev => this.setPropValue(name, ev.target.value)}>{
+                                options.map((option, key) => <option key={key} value={option.value}>{option.label}</option>)
+                            }</select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ];
+    }
+
+    BmRadio(element) {
+        const { name, label, css, options } = element;
+
+        return [
+            <Label key="1" label={label} size="" alignment={this.props.alignment} />,
+            <div key="2" className="field-body">
+                <div className="field">
+                    <div className="control">
+                        {
+                            options.map((option, key) => (
+                                <label key={key} className="radio">
+                                    <input name={name} className={css} type="radio"
+                                        value={option.value}
+                                        checked={this.data[name] === option.value}
+                                        onChange={() => this.setPropValue(name, option.value)} />
+                                    {option.label}
+                                </label>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        ];
+    }
+
+    BmDatePicker(element) {
+        const { name, label, css } = element;
+
+        return [
+            <Label key="1" label={label} alignment={this.props.alignment} />,
+            <div key="2" className="field-body">
+                <div className="field">
+                    <div className="control">
+                        <input name={name} className={`input ${css}`} type="text" />
+                    </div>
+                </div>
+            </div>
+        ];
+    }
+
+    BmTextarea(element) {
+        const { name, label, css, placeholder } = element;
+
+        return [
+            <Label key="1" label={label} alignment={this.props.alignment} />,
+            <div key="2" className="field-body">
+                <div className="field">
+                    <div className="control">
+                        <textarea name={name} className={`textarea ${css}`} type="text" placeholder={placeholder}
+                            value={this.data[name]} onChange={ev => this.setPropValue(name, ev.target.value)} />
+                    </div>
+                </div>
+            </div>
+        ];
+    }
+
+    genElem(elem) {
+        /**
+         * element wrapper
+         * @param {object} element Element
+         * @returns {object} element vdom
+         */
+        const elemWrapper = (element) => {
+            switch (element.type) {
+            case 'text':
+                return this.BmText(element);
+            case 'checkbox':
+                return this.BmCheck(element);
+            case 'select':
+                return this.BmSelect(element);
+            case 'radio':
+                return this.BmRadio(element);
+            case 'datepicker':
+                return this.BmDatePicker(element);
+            case 'textarea':
+                return this.BmTextarea(element);
+            default:
+                break;
+            }
+            throw new Error('Unrecognized element type!');
+        };
+
+        return (
+            <div className={`field ${this.props.alignment || ''}`}>
+                {elemWrapper(elem)}
+            </div>
+        );
+    }
+
+    genRow(row) {
+        return row.map((elem, key) => (
+            <div key={key} className="column">
+                {this.genElem(elem)}
+            </div>
+        ));
+    }
+
+    genForm(fields) {
+        return fields.map((row, key) => (
+            <div key={key} className="columns is-2">
+                {this.genRow(row)}
+            </div>
+        ));
+    }
+
+    render() {
+        const { name, fields } = this.props;
+        return (
+            <form name={name} onSubmit={this.props.onSubmit}>
+                {this.genForm(fields)}
+            </form>
+        );
+    }
+});
+
+export default BuForm;
