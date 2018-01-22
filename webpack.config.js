663519290edc813f2cdaa154482a9fb3d5b591c1
Simon Li <lsimon@ca.ibm.com>
2018-01-21 22:32:08 -0500

    optimized bundle size by excluding  imported packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,53 @@
-var path = require('path');
-module.exports = {
-  entry: './src/buForm.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'buForm.js',
-    libraryTarget: 'commonjs2'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
-      }
-    ]
-  },
-  externals: {
-    'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-  }
+const webpack = require('webpack');
+const path = require('path');
+
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const env = process.env.WEBPACK_ENV;
+
+const libraryName = '[name]'; // in case that needs common modules to be built
+const plugins = [];
+let outputFile;
+
+if (env === 'build') {
+    plugins.push(new UglifyJsPlugin({
+        minimize: true
+    }));
+    outputFile = `${libraryName}.min.js`;
+}
+else {
+    outputFile = `${libraryName}.js`;
+}
+
+const config = {
+    entry: {
+        buForm: path.resolve(__dirname, './src/buForm.js'),
+        // can add more entries, i.e. common chunks for vendor modules
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    externals: {
+        react: 'react',
+        mobx: 'mobx',
+        'mobx-react': 'mobx-react'
+    },
+    plugins
 };
+
+module.exports = config;
